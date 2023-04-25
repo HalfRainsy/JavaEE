e615de7042653ccae3cd127bae5192b7a7f2e4d0
TimJung <50062127+HalfRainsy@users.noreply.github.com>
2023-04-25 21:22:36 +0800

    1

--- ArrayList_Test/src/com/yty/demo1/ArrayList_1.java
+++ ArrayList_Test/src/com/yty/demo1/ArrayList_1.java
@@ -12,5 +12,26 @@ package com.yty.demo1;
 public class ArrayList_1 {
     public static void main(String[] args) {
         System.out.println("hahahaha");
+        //数据类型变量、
+
+        int num1=10;
+        int num2=20;
+        //声明数组
+        double[] princes;
+        //数组初始化  静态数组
+        princes= new double[]{20,31,44,76};//赋值
+        /*
+
+        String[] foods;
+        foods=new String[]{"拌海蜇","小猪盖被","烧鸡","烤鹅"};
+        */
+
+        //数组的声明和初始化   动态数组  以及初始化
+        //数组一旦初始化完成，长度确定且不能更改。
+        String[] foods;
+        foods=new String[4];
+        System.out.println(foods);
+
+
     }
 }
---
+++ ArrayList_Test/src/com/yty/demo1/ArrayList_2.java
@@ -0,0 +1,16 @@
+package com.yty.demo1;
+
+/**
+ * ClassName: ArrayList_2
+ * Package: com.yty.demo1
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/4/25 21:12
+ * @Version 1.0
+ */
+public class ArrayList_2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
