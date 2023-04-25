a5313bc83cc5dfc426226c69cbc07b1e9e572b0e
TimJung <50062127+HalfRainsy@users.noreply.github.com>
2023-04-25 21:04:36 +0800

    first commit

---
+++ ArrayList_Test/src/com/yty/demo1/ArrayList_1.java
@@ -0,0 +1,16 @@
+package com.yty.demo1;
+
+/**
+ * ClassName: ArrayList_1
+ * Package: com.yty.demo1
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/4/25 21:02
+ * @Version 1.0
+ */
+public class ArrayList_1 {
+    public static void main(String[] args) {
+        System.out.println("hahahaha");
+    }
+}
