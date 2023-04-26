7550f175bbb4f0e8ceb731b2f63d21bd018314ce
TimJung <2275845986@qq.com>
2023-04-26 13:08:28 +0800

    测试一个上传-1

--- ArrayList_Test/src/com/yty/demo1/test.java
+++ ArrayList_Test/src/com/yty/demo1/test.java
@@ -12,5 +12,6 @@ package com.yty.demo1;
 public class test {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println("zheshi yige ceshi lei ");
     }
 }
