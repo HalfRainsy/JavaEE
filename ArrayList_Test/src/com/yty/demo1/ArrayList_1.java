b9c6c51631152e0919e4eb33290c697d2a848753
TimJung <2275845986@qq.com>
2023-04-26 22:28:46 +0800

    2023/04/26学习进程

--- ArrayList_Test/src/com/yty/demo1/ArrayList_1.java
+++ ArrayList_Test/src/com/yty/demo1/ArrayList_1.java
@@ -20,21 +20,35 @@ public class ArrayList_1 {
         double[] princes;
         //数组初始化  静态数组
         princes= new double[]{20,31,44,76};//赋值
-        /*
+
 
         String[] foods;
         foods=new String[]{"拌海蜇","小猪盖被","烧鸡","烤鹅"};
-        */
+        System.out.println(foods[2]);
 
         //数组的声明和初始化   动态数组  以及初始化
         //数组一旦初始化完成，长度确定且不能更改。
-        String[] foods;
+        /*String[] foods;
         foods=new String[4];
+        foods[0]="拌海蜇";
+        foods[1]="小猪盖被";
+        foods[1]="烧鸡";
+        foods[1]="烤鹅";
         System.out.println(foods);
+
+         */
         int arr[]=new int[4];
         int[] arr1={1234};
         //数组元素的调用
-
+        System.out.println(arr1[0]);
+        //数组的长度  用来描述数组中容量的大小
+        //使用length属性表示
+        System.out.println(arr1.length);
+        //如何遍历数组
+        for (int i=0;i<foods.length;i++){
+            System.out.println(foods[i]);
+        }
+        //fori.  //for循环快捷键
 
     }
 }
--- ArrayList_Test/src/com/yty/demo1/ArrayList_2.java
+++ ArrayList_Test/src/com/yty/demo1/ArrayList_2.java
@@ -11,9 +11,45 @@ package com.yty.demo1;
  */
 public class ArrayList_2 {
     public static void main(String[] args) {
-        System.out.println();
+       //数组元素的默认初始化值
+        int[]  arr1=new int[3];
+        System.out.println(arr1[0]);
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+        //根据不同的数组类型而分
+        //整型
+        //浮点型认初始化值：0.0
+        double[] arr3=new double[5];
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.println(arr3[i]);
+        }
+        //字符型 默认初始化值：0   or 理解为： \u00000
+
+        char[] arr44=new char[4];
+        if (arr44[0]==0){
+            System.out.println("是数字0");
+        }
+        if (arr44[0]=='0'){
+            System.out.println("是字符0");
+        }
+        if (arr44[0]=='\u0000'){
+            System.out.println("是字符02");
+        }
+        for (int i = 0; i < arr44.length; i++) {
+            System.out.println(arr44[i]);
+        }
 
 
-        System.out.println("zheshi yige cweshi de ");
+        //Boolean  默认数值类型：false
+        boolean[] arr5=new boolean[4];
+        for (int i = 0; i < arr5.length; i++) {
+            System.out.println("boolea:"+arr5[i]);
+        }
+        //引用数据类型 m默认数值为：null
+        String[] arr6=new String[10];
+        for (int i = 0; i < arr6.length; i++) {
+            System.out.println("String:"+arr6[i]);
+        }
     }
 }
