73c7b0ac5e933277fb586de4c4cddd63ff22c0b5
TimJung <2275845986@qq.com>
2023-05-12 12:25:42 +0800

    face to project

---
+++ Oop1/src/demo1.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: demo1
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/5/10 21:53
+ * @Version 1.0
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        //面向对象 oop   面向过程  pop
+    }
+}
