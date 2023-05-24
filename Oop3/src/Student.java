0c32ec24a1c53c78430cecbdf7b6f020b020502f
TimJung <2275845986@qq.com>
2023-05-24 22:51:27 +0800

    t

---
+++ Oop3/src/Student.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;/**
+* ClassName: Student
+* Package: PACKAGE_NAME
+* Description: 
+* @Author Tim
+* @Create 2023/5/24 21:53 
+* @Version 1.0   
+*/public class Student {
+}
---
+++ Oop3/src/StudentTest.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;/**
+* ClassName: StudentTest
+* Package: PACKAGE_NAME
+* Description: 
+* @Author Tim
+* @Create 2023/5/24 21:54 
+* @Version 1.0   
+*/public class StudentTest {
+}
