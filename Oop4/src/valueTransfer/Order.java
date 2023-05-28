9e7c4471c2bae19e05a1dd716c42709ed7769be1
TimJung <2275845986@qq.com>
2023-05-28 21:52:07 +0800

    1

---
+++ Oop4/src/valueTransfer/Order.java
@@ -0,0 +1,9 @@
+package valueTransfer;/**
+* ClassName: Order
+* Package: valueTransfer
+* Description: 
+* @Author Tim
+* @Create 2023/5/28 21:47 
+* @Version 1.0   
+*/public class Order {
+}
---
+++ Oop4/src/valueTransfer/ValueTransfer.java
@@ -0,0 +1,9 @@
+package valueTransfer;/**
+* ClassName: ValueTransfer
+* Package: valueTransfer
+* Description: 
+* @Author Tim
+* @Create 2023/5/28 21:43 
+* @Version 1.0   
+*/public class ValueTransfer {
+}
