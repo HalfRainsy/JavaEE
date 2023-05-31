4f2a899ab0a6b8ccbd5923d5c0f5a8d72f8cc493
TimJung <2275845986@qq.com>
2023-05-31 22:59:51 +0800

    封装2

---
+++ Oops5/src/Person.java
@@ -0,0 +1,39 @@
+/**
+ * ClassName: Person
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/5/31 22:57
+ * @Version 1.0
+ */
+public class Person {
+    private String name;
+    private int age;
+    private char gender;
+    private int tall;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getTall() {
+        return tall;
+    }
+
+    public void setTall(int tall) {
+        this.tall = tall;
+    }
+}
