0c1f6292bf0fd5e845d88e65ddde73c1d8b9cf5b
TimJung <2275845986@qq.com>
2023-06-05 22:32:12 +0800

    封装

--- Oops6/src/CustomerTest.java
+++ Oops6/src/CustomerTest.java
@@ -10,7 +10,23 @@
 public class CustomerTest {
     public static void main(String[] args) {
         Customer cm = new Customer("Jane", "Smith");
-        Account an = new Account(1000, 2000, 0.0123);
+//        Account an = new Account(1000, 2000, 0.0123);
+//        cm.setAccount(an);
+        cm.setAccount(new Account(1000, 2000, 0.0123));//匿名对象
 
+        /*
+        关于匿名对象
+        new Account(1000, 2000, 0.0123).withdraw(1000);取钱操作
+        匿名对象往往只能被调用一次
+        匿名对象往往作为
+         */
+        //针对于客户的账户进行取钱，存钱的操作
+        cm.getAccount().deposit(100);
+        cm.getAccount().deposit(960);
+        cm.getAccount().withdraw(2000);
+
+        System.out.println("客户【" + cm.getLastname() + "】," + cm.getFirstname() +
+                ",[有一个账户：" + cm.getAccount().getId() + "年利率为：" + cm.getAccount().
+                getAnnualInterestRate() * 100 + "%,余额为：" + cm.getAccount().getBalance());
     }
 }
