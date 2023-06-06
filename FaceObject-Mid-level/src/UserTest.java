3e3995811747fcb2ef9a6dfbc037eb1b1795e5c7
TimJung <2275845986@qq.com>
2023-06-06 22:38:04 +0800

    面向对象7-1-01

--- FaceObject-Mid-level/src/UserTest.java
+++ FaceObject-Mid-level/src/UserTest.java
@@ -8,23 +8,24 @@
  * @Version 1.0
  */
 public class UserTest {
+    User u=new User();
 }
 
-class user {
+class User {
     String name;
     int age;
 
-    public user() {
+    public User() {
         //模拟对象创建时，需要初始化
     }
 
-    public user(String name) {
+    public User(String name) {
         this();//表示public user() 方法
         //模拟对象创建时，需要初始化
         this.name = name;
     }
 
-    public user(String name, int age) {
+    public User(String name, int age) {
         this(name);//调用public user(String name)方法
         //模拟对象创建时，需要初始化
         this.name = name;
