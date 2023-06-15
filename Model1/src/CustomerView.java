c8f6b8250ac0733a1d29f669cc97f840030cb6a9
TimJung <2275845986@qq.com>
2023-06-15 22:36:11 +0800

    项目

--- Model1/src/CustomerView.java
+++ Model1/src/CustomerView.java
@@ -14,27 +14,39 @@ public class CustomerView {
     CustomerList customerList = new CustomerList(10);
 
     public void enterMainMenu() {
+        boolean isFlag=true;
+        while(isFlag){
+            System.out.println("\n---------------管理系统-----------------------------");
+            System.out.println("                  1：添加客户                  ");
+            System.out.println("                  2：修改客户                  ");
+            System.out.println("                  3：删除客户                  ");
+            System.out.println("                  4：客户列表                  ");
+            System.out.println("                  5：退出    \n                ");
+            System.out.println("                   请选择1--5：                 ");
+
+
+            isFlag=false;
+        }
+    }
+
+    private void addNewCustomer() {
 
     }
 
-    private void addNewCustomer(){
+    private void modifyCustomer() {
 
     }
 
-    private void modifyCustomer(){
+    private void deleteCustomer() {
 
     }
 
-    private void deleteCustomer(){
-
-    }
-
-    private void ListAllCustomer(){
+    private void ListAllCustomer() {
 
     }
 
     public static void main(String[] args) {
-
+        CustomerView customerView = new CustomerView();
     }
 
 }
