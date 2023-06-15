0ef10facded490f91931ed0a51e811e2ddca9dfa
TimJung <2275845986@qq.com>
2023-06-15 22:31:24 +0800

    项目

--- Model1/src/CustomerView.java
+++ Model1/src/CustomerView.java
@@ -1,3 +1,5 @@
+import jdk.jfr.internal.tool.Main;
+
 /**
  * ClassName: CustomerView
  * Package: PACKAGE_NAME
@@ -8,4 +10,31 @@
  * @Version 1.0
  */
 public class CustomerView {
+
+    CustomerList customerList = new CustomerList(10);
+
+    public void enterMainMenu() {
+
+    }
+
+    private void addNewCustomer(){
+
+    }
+
+    private void modifyCustomer(){
+
+    }
+
+    private void deleteCustomer(){
+
+    }
+
+    private void ListAllCustomer(){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
