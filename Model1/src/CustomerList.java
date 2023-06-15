ff417c8eb6ca609e06d9c27eabf8c87ed89bc056
TimJung <2275845986@qq.com>
2023-06-15 22:06:53 +0800

    项目

---
+++ Model1/src/CustomerList.java
@@ -0,0 +1,31 @@
+/**
+ * ClassName: CustomerList
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/6/15 22:01
+ * @Version 1.0
+ */
+public class CustomerList {
+    private Customer[] customers;
+    private int total;//记录保存客户数量的对象
+    public CustomerList(int totalCustomer){
+
+    }
+    public boolean addCustomer(Customer customer){
+
+    }
+    public boolean deleteCustomer(int index){
+
+    }
+    public Customer[] getAllCustomers(){
+
+    }
+    public Customer getCustomer(int index){
+
+    }
+    public int getTotal(){
+
+    }
+}
