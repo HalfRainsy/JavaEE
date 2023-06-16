545ba18c9258b0f33e6504de7ebd3e5cb300d0ad
TimJung <2275845986@qq.com>
2023-06-16 22:29:38 +0800

    实操

---
+++ FaceToProject_02/src/ExtendsTest.java
@@ -0,0 +1,21 @@
+/**
+ * ClassName: ExtendsTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/6/16 22:27
+ * @Version 1.0
+ */
+public class ExtendsTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.name = "tim";
+        p1.eat();
+        Student s1 = new Student();
+        s1.name = "Tom";
+        s1.sleep();
+
+
+    }
+}
--- FaceToProject_02/src/Student.java
+++ FaceToProject_02/src/Student.java
@@ -7,18 +7,19 @@
  * @Create 2023/6/16 22:26
  * @Version 1.0
  */
-public class Student {
-    String name;
-    int age;
+public class Student extends Person {
+    //    String name;
+//    int age;
     String school;
-    public void study(){
+
+    public void study() {
         System.out.println("人学习");
     }
 
-    public void eat(){
-        System.out.println("人吃");
-    }
-    public void sleep(){
-        System.out.println("人睡觉");
-    }
+//    public void eat(){
+//        System.out.println("人吃");
+//    }
+//    public void sleep(){
+//        System.out.println("人睡觉");
+//    }
 }
