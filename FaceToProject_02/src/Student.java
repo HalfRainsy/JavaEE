40d152caa5b7ef25b34e29f2e3d375a3b372f0b7
TimJung <2275845986@qq.com>
2023-06-16 22:27:13 +0800

    实操

---
+++ FaceToProject_02/src/Student.java
@@ -0,0 +1,24 @@
+/**
+ * ClassName: Student
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/6/16 22:26
+ * @Version 1.0
+ */
+public class Student {
+    String name;
+    int age;
+    String school;
+    public void study(){
+        System.out.println("人学习");
+    }
+
+    public void eat(){
+        System.out.println("人吃");
+    }
+    public void sleep(){
+        System.out.println("人睡觉");
+    }
+}
