956e1011663c49ef4b88766fa32b5e1cbae46b43
Tim <2275845986@qq.com>
2023-06-19 22:38:06 +0800

    code5

---
+++ Super/src/Person.java
@@ -0,0 +1,23 @@
+/**
+ * ClassName: Person
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/6/16 22:25
+ * @Version 1.0
+ */
+public class Person {
+    String name;
+    int age;
+
+    public void eat() {
+        System.out.println("人吃");
+    }
+
+    public void sleep() {
+        System.out.println("人睡觉");
+    }
+
+   
+}
