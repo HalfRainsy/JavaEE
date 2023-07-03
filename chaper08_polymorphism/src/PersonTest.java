b5b3d897cc26040a94b6281cf6eb2fa726171980
Tim <2275845986@qq.com>
2023-07-03 22:36:44 +0800

    多态2

--- chaper08_polymorphism/src/PersonTest.java
+++ chaper08_polymorphism/src/PersonTest.java
@@ -13,7 +13,16 @@ public class PersonTest {
         Person p1 = new Person();
         //多态性：为什么要用多态性？----遗留问题
         Person p2 = new Man();//注意  Man一定得是继承Person
-
+        /*
+        多态的应用：虚拟方法调用
+        在多态的场景下，调用方法时
+            编译时认为方法是左边声明的时父类的类型，
+            执行时，实际执行的是子类重写父类的方法。
+            简称为：编译看左边，运行看右边
+         */
+        p2.eat();
+        //测试属性的是否满足多态性？
+        System.out.println(p2.name);
 
     }
 }
