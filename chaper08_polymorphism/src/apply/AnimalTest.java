c6efc5596ef58a622242ae558b856acea6365ada
Tim <2275845986@qq.com>
2023-07-04 22:20:23 +0800

    多态3

---
+++ chaper08_polymorphism/src/apply/AnimalTest.java
@@ -0,0 +1,61 @@
+package apply;
+
+/**
+ * ClassName: AnimalTest
+ * Package: apply
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/7/4 22:12
+ * @Version 1.0
+ */
+public class AnimalTest {
+    public static void main(String[] args) {
+        AnimalTest an=new AnimalTest();
+        an.func(new Cat());
+    }
+
+    public void func(Animal animal) {
+
+        animal.eat();
+        animal.jump();
+
+    }
+}
+
+class Animal {
+    public void eat() {
+        System.out.println("动物进食");
+    }
+
+    public void jump() {
+        System.out.println("动物跳");
+    }
+}
+
+class Dog extends Animal {
+    public void eat() {
+        System.out.println("狗进食");
+    }
+
+    public void jump() {
+        System.out.println("狗跳墙");
+    }
+
+    public void watchDoor() {
+        System.out.println("看家");
+    }
+}
+
+class Cat extends Animal {
+    public void eat() {
+        System.out.println("猫进fish");
+    }
+
+    public void jump() {
+        System.out.println("猫跳树");
+    }
+    public void catchM(){
+        System.out.println("抓老鼠");
+    }
+}
