cdc9651c94dff81bc4f67dc2c167c40d4237a4b4
Tim <2275845986@qq.com>
2023-07-23 22:10:37 +0800

    1

--- chapte_ToString/src/ToStringTest.java
+++ chapte_ToString/src/ToStringTest.java
@@ -1,9 +1,28 @@
-package PACKAGE_NAME;/**
-* ClassName: ToStringTest
-* Package: PACKAGE_NAME
-* Description: 
-* @Author Tim
-* @Create 2023/7/23 22:01 
-* @Version 1.0   
-*/public class ToStringTest {
+/**
+ * ClassName: ToStringTest
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/7/23 22:01
+ * @Version 1.0
+ */
+public class ToStringTest {
+    public static void main(String[] args) {
+        User u1=new User("Tom",12);
+        System.out.println(u1.toString());
+    }
+
+}
+class User{
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
 }
