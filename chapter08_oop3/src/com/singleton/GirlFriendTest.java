c6a717a5fe85cdf069772b2e1d353059158d22a4
Tim <2275845986@qq.com>
2023-10-01 09:10:57 +0800

    懒汉式--单例模式

--- chapter08_oop3/src/com/singleton/GirlFriendTest.java
+++ chapter08_oop3/src/com/singleton/GirlFriendTest.java
@@ -10,8 +10,25 @@ package com.singleton;
  * @Version 1.0
  */
 public class GirlFriendTest {
+    public static void main(String[] args) {
+
+    }
 }
-
+//懒汉式
 class GirlFriend {
+    //1.类的构造器私有化
+    private  GirlFriend(){
 
+    }
+    //2.声明当前类的实例
+    private static  GirlFriend instance=null;
+    //3.通过get方法获取当前类的实例，如果未创建方法，则在类的内部进行创建
+    //4.此属性也声明为static。
+    public static GirlFriend getInstance(){
+        if(instance==null){
+            instance=new GirlFriend();
+
+        }
+        return instance;
+    }
 }
