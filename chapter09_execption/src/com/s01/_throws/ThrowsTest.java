5d852920ddfbb9b60db2eb82054a905983edf92c
Tim <2275845986@qq.com>
2023-10-25 08:59:00 +0800

    异常

---
+++ chapter09_execption/src/com/s01/_throws/ThrowsTest.java
@@ -0,0 +1,19 @@
+package com.s01._throws;
+
+import com.sun.javafx.iio.ios.IosDescriptor;
+
+/**
+ * ClassName: ThrowsTest
+ * Package: com.s01._throws
+ * Description:
+ *
+ * @Author Tim
+ * @Create 2023/10/24 10:39
+ * @Version 1.0
+ */
+public class ThrowsTest {
+
+    public void method() throws NullPointerException {
+    //准对编译时异常
+    }
+}
